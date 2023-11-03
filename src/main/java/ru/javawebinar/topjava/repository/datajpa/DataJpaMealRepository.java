64d75526a3803eac2335b04148bb8f5b67e8b4f6
Anna Kirillova <a.kirillova1812@gmail.com>
2023-11-03 13:35:53 +0400

    DataJpaMealRepository implementation

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -2,42 +2,65 @@ package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class DataJpaMealRepository implements MealRepository {
 
-    private final CrudMealRepository crudRepository;
+    private final CrudMealRepository crudMealRepository;
+    private final CrudUserRepository crudUserRepository;
 
-    public DataJpaMealRepository(CrudMealRepository crudRepository) {
-        this.crudRepository = crudRepository;
+    public DataJpaMealRepository(CrudMealRepository crudMealRepository, CrudUserRepository crudUserRepository) {
+        this.crudMealRepository = crudMealRepository;
+        this.crudUserRepository = crudUserRepository;
     }
 
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        Optional<User> user = crudUserRepository.findById(userId);
+
+        if (user.isEmpty()) {
+            return null;
+        }
+        meal.setUser(user.get());
+        if (meal.isNew()) {
+            crudMealRepository.save(meal);
+            return meal;
+        }
+
+        return get(meal.id(), userId) != null ? crudMealRepository.save(meal) : null;
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return crudMealRepository.delete(id, userId) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        Optional<User> user = crudUserRepository.findById(userId);
+        return user.isPresent() ? crudMealRepository.findByIdAndUser(id, user.get()) : null;
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        Optional<User> user = crudUserRepository.findById(userId);
+        return user.isPresent() ? crudMealRepository.findAllByUserOrderByDateTimeDesc(user.get()) : new ArrayList<>();
+
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+//        return crudMealRepository.getBetweenHalfOpen(startDateTime, endDateTime, userId);
+        Optional<User> user = crudUserRepository.findById(userId);
+        return user.isPresent()
+                ? crudMealRepository.findAllByUserAndDateTimeGreaterThanEqualAndDateTimeLessThanOrderByDateTimeDesc(user.get(), startDateTime, endDateTime)
+                : new ArrayList<>();
     }
 }
