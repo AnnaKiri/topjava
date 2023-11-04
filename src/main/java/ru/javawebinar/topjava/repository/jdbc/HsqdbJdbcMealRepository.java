68876370a434f3f9d51b92c8c70d81d77fd7bbb4
Anna Kirillova <a.kirillova1812@gmail.com>
2023-11-04 13:27:26 +0400

    Separation to Postgres and HSQLDB for JdbcMealRepository

---
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/HsqdbJdbcMealRepository.java
@@ -0,0 +1,34 @@
+package ru.javawebinar.topjava.repository.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.repository.MealRepository;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static ru.javawebinar.topjava.Profiles.HSQL_DB;
+
+@Repository
+@Profile(HSQL_DB)
+public class HsqdbJdbcMealRepository extends AbstractJdbcMealRepository<Timestamp> {
+
+    public HsqdbJdbcMealRepository(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        super(jdbcTemplate, namedParameterJdbcTemplate);
+    }
+
+    @Override
+    protected Timestamp getTransformedLocalDateTime(LocalDateTime dateTime) {
+        return Timestamp.valueOf(dateTime);
+    }
+}
---
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/PostgresJdbcMealRepository.java
@@ -0,0 +1,33 @@
+package ru.javawebinar.topjava.repository.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.repository.MealRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static ru.javawebinar.topjava.Profiles.POSTGRES_DB;
+
+@Repository
+@Profile(POSTGRES_DB)
+public class PostgresJdbcMealRepository extends AbstractJdbcMealRepository<LocalDateTime> {
+
+    public PostgresJdbcMealRepository(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        super(jdbcTemplate, namedParameterJdbcTemplate);
+    }
+
+    @Override
+    protected LocalDateTime getTransformedLocalDateTime(LocalDateTime dateTime) {
+        return dateTime;
+    }
+}
