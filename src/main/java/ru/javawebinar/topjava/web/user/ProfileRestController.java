f661052ec5cf334d4e31ce80e8fe062741a1d6d7
Anna Kirillova <a.kirillova1812@gmail.com>
2023-12-20 14:06:15 +0400

    Refactoring user controllers

--- src/main/java/ru/javawebinar/topjava/web/user/ProfileRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/ProfileRestController.java
@@ -1,15 +1,12 @@
 package ru.javawebinar.topjava.web.user;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.to.UserTo;
-import ru.javawebinar.topjava.util.validator.UserValidator;
 
 import javax.validation.Valid;
 import java.net.URI;
@@ -20,14 +17,6 @@ import static ru.javawebinar.topjava.web.SecurityUtil.authUserId;
 @RequestMapping(value = ProfileRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
 public class ProfileRestController extends AbstractUserController {
     static final String REST_URL = "/rest/profile";
-
-    @Autowired
-    private UserValidator userValidator;
-
-    @InitBinder
-    private void initBinder(WebDataBinder binder) {
-        binder.addValidators(userValidator);
-    }
 
     @GetMapping
     public User get() {
